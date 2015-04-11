ca3e20f0c77d82d6d01d49b7eb0b4e9c9b5da613
Han Koh <rhgksrua@gmail.com>
2015-04-10 20:07:56 -0500

    renamed files and moved files

---
+++ src/playingcards.js
@@ -0,0 +1,65 @@
+function PlayingCards() {
+
+    this.cards = [];
+    this.num = 0;
+    return this;
+}
+
+
+PlayingCards.prototype = {
+
+    create: function(num) {
+
+        this.num = num;
+        
+        // reset cards and make new set of decks
+        this.cards = [];
+        var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        var suits = ['C', 'D', 'H', 'S'];
+        var deck = [];
+        num = num || 1;
+        // deck hold a single complete set
+        ranks.forEach(function(rank) {
+            suits.forEach(function(suit) {
+                deck.push(rank + suit);
+            });
+        });
+        
+        // Create decks with num of single deck
+        for (i = 0; i < num; i++) {
+            this.cards = this.cards.concat(deck);
+        }
+        return this;
+        
+    },
+    
+    shuffle: function() {
+        
+        var shuffled = [];
+        var index;
+        
+        while(this.cards.length > 0) {
+            // random index to remove
+            index = Math.floor(Math.random() * this.cards.length);
+            // store randomly picked card to shuffled
+            shuffled = shuffled.concat(this.cards.splice(index, 1));
+            
+            
+        }
+        this.cards = shuffled;
+        return this
+    },
+    
+    deal: function(num) {
+        num = num || 1;
+        if (num < 1 || isNaN(num)) {
+            throw new Error('Number must be greater than 0');
+        }
+        return this.cards.splice(0, num);
+    },
+    
+    count: function() {
+        return this.cards.length;
+    }
+};
+
